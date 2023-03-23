4dc85b864c287b1f2110f788513eb0194093924e
Yuvraj singh <113610724+Yuvi83@users.noreply.github.com>
2023-03-23 23:54:02 +0530

    Add files via upload

---
+++ 11script.js
@@ -0,0 +1,11 @@
+document.write("Welcome to Practically"+"<br>");
+window.alert("hello!");
+ document.getElementById("demo2").innerHTML ="helloworld";
+//console.log(5+5);
+var x=10;              /*var x=10;y=20;z*/
+ var y=20;
+// const a=30 ; //constant are fixed if one time give value then cant change value of constant.
+var z;
+ z =x+y;
+ document.write("the sum of x and y is "+z+" .");
+console.log("hi");//for debugging purpose you can call the console.log()method in the browser to display the data.
